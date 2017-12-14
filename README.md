e3ed3d90c01adac4e2e8b9753c1a8f9f7ce043d1
Tyler Wanek <tylerw@axosoft.com>
2017-12-14 08:31:37 -0700

    Implement simple buildtime script

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-Node-Build-Time
+#Node-Build-Time
+
+## Installation
+```
+yarn global add buildtime
+```
+or
+```
+npm i -g buildtime
+```
+
+## Usage
+Navigate to the target module and use the `buldtime` command in cli.
---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+const fs = require('fs');
+
+try {
+  const rawContents = fs.readFileSync('package.json', 'utf8');
+  const package = JSON.parse(rawContents);
+  package.buildTime = new Date();
+  fs.writeFileSync('package.json', JSON.stringify(package));
+  process.exit(0);
+} catch (error) {
+  console.error('BuildTime: Unable to write to package.json.');
+  process.exit(1);
+}
